01592d9c15aefd98edd3e017506fabbd42b9a197
AssilAttiah <asat0001@stud.kea.dk>
2024-05-13 11:09:03 +0200

    Initial commit

---
+++ src/main/java/com/example/bilabo_intranet/BilAboIntranetApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilabo_intranet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilAboIntranetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilAboIntranetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bilabo_intranet/BilAboIntranetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilabo_intranet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilAboIntranetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
