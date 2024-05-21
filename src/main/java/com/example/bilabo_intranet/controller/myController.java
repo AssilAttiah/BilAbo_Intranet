6b133cfcdfdcb714403eefc436fb8f23d4337347
AssilAttiah <asat0001@stud.kea.dk>
2024-05-21 10:49:53 +0200

    if() statements i myController.java til login

--- src/main/java/com/example/bilabo_intranet/controller/myController.java
+++ src/main/java/com/example/bilabo_intranet/controller/myController.java
@@ -2,6 +2,8 @@ package com.example.bilabo_intranet.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class myController {
@@ -10,4 +12,18 @@ public class myController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(value="loginType", defaultValue = "")String loginType) {
+        if (loginType.equals("Medarbejder")) {
+            return "dataregistrering";
+        } else if (loginType.equals("FDM")) {
+            return "dataregistrering";
+        } else if (loginType.equals("Admin")) {
+            return "forretning";
+        } else {
+            return "Forkert login";
+        }
+    }
 }
+
