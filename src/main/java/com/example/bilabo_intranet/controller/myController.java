6b3e7181e7ef8a4b4f83d1a7c134482299da1133
FrederikRudHansen <147395022+FrederikRudHansen@users.noreply.github.com>
2024-05-21 12:59:54 +0200

    login ik færdig

--- src/main/java/com/example/bilabo_intranet/controller/myController.java
+++ src/main/java/com/example/bilabo_intranet/controller/myController.java
@@ -22,33 +22,7 @@ public class myController {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String login(
-            @RequestParam("loginType") String loginType,
-            @RequestParam("brugernavn") String username,
-            @RequestParam("adgangskode") String password,
-            Model model) {
 
-        if ("FDM".equalsIgnoreCase(loginType) && "fdmuser".equals(username) && "fdmpass".equals(password)) {
-            return "redirect:/report";
-        } else if (("Medarbejder".equalsIgnoreCase(loginType) || "Admin".equalsIgnoreCase(loginType))
-                && "user".equals(username) && "pass".equals(password)) {
-            return "redirect:/dataregistrering";
-        } else {
-            model.addAttribute("error", "Invalid username or password");
-            return "login";
-        }
-    }
-
-    @GetMapping("/dataregistrering")
-    public String dataregistrering() {
-        return "dataregistrering";
-    }
-
-    @GetMapping("/raport")
-    public String report() {
-        return "raport";
-    }
 }
 
 
