45330c138e29164965894ab92b151da72a1916bc
FrederikRudHansen <147395022+FrederikRudHansen@users.noreply.github.com>
2024-05-21 10:54:48 +0200

    login ik færdig

--- src/main/java/com/example/bilabo_intranet/controller/myController.java
+++ src/main/java/com/example/bilabo_intranet/controller/myController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class myController {
@@ -13,12 +15,41 @@ public class myController {
 @Autowired
     BilService bilService;
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
+
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
     }
 
+    @PostMapping("/login")
+    public String login(
+            @RequestParam("loginType") String loginType,
+            @RequestParam("brugernavn") String username,
+            @RequestParam("adgangskode") String password,
+            Model model) {
 
+        if ("FDM".equalsIgnoreCase(loginType) && "fdmuser".equals(username) && "fdmpass".equals(password)) {
+            return "redirect:/report";
+        } else if (("Medarbejder".equalsIgnoreCase(loginType) || "Admin".equalsIgnoreCase(loginType))
+                && "user".equals(username) && "pass".equals(password)) {
+            return "redirect:/dataregistrering";
+        } else {
+            model.addAttribute("error", "Invalid username or password");
+            return "login";
+        }
+    }
 
+    @GetMapping("/dataregistrering")
+    public String dataregistrering() {
+        return "dataregistrering";
+    }
 
+    @GetMapping("/raport")
+    public String report() {
+        return "raport";
+    }
 }
+
+
+
