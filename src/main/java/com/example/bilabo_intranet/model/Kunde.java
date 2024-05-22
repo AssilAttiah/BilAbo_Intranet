330f942f9f15f11debe04936a5b6fe1de6e9ed2a
Jonas Vistrup <Jonas_vistrup@live.dk>
2024-05-22 13:17:48 +0200

    Lavet SQL til Kunde og sørget for den gemmer data.

--- src/main/java/com/example/bilabo_intranet/model/Kunde.java
+++ src/main/java/com/example/bilabo_intranet/model/Kunde.java
@@ -1,7 +1,49 @@
 package com.example.bilabo_intranet.model;
 
 public class Kunde {
-    String navn;
-    String efternavn;
-    int korekortnr;
+    private int kundeID;
+    private String fornavn;
+    private String efternavn;
+    private int alder;
+    private String kørekortnummer;
+
+    public int getKundeID() {
+        return kundeID;
+    }
+
+    public void setKundeID(int kundeID) {
+        this.kundeID = kundeID;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEfternavn() {
+        return efternavn;
+    }
+
+    public void setEfternavn(String efternavn) {
+        this.efternavn = efternavn;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public void setAlder(int alder) {
+        this.alder = alder;
+    }
+
+    public String getKørekortnummer() {
+        return kørekortnummer;
+    }
+
+    public void setKørekortnummer(String kørekortnummer) {
+        this.kørekortnummer = kørekortnummer;
+    }
 }
